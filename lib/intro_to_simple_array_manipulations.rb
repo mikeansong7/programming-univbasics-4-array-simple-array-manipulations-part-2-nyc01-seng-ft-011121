f67ae4de452ab2c9698c8502cc8219c4bc28b6ec
Michael B Ansong <michael.ansong7@gmail.com>
2020-12-28 18:56:40 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -7,5 +7,17 @@ def using_insert (arrays,arrayz)
 end
   
   def using_uniq(array)
-    
-  
+    array.uniq
+end
+
+def using_flatten (array)
+  array.flatten
+end
+
+def using_delete (array, string)
+  array.delete(string)
+end
+
+def using_delete_at (array, integer)
+  array.delete_at(integer)
+end
